ea0783f3ce834695969eaa70c17ca62a824e2b49
flakster <carlos.santamaria@gmail.com>
2020-03-09 09:55:55 -0500

    move print and put statements to main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,39 @@
 #!/usr/bin/env ruby
-require_relative '../lib/interface'
 require_relative '../lib/game'
+
+def digit?(pos)
+  /\A\d\z/.match(pos)
+end
+
+def read_position(game)
+  pos = 0
+  loop do
+    pos = gets.chomp
+    if !digit?(pos)
+      print 'Please enter a number between 1 and 9 (or 0 to exit): '
+    elsif game.board.taken?(pos)
+      print 'That position is already taken, please choose an empty one: '
+    else
+      break
+    end
+  end
+  pos
+end
+
+def board_render(game)
+  (1..81).each do |i|
+    print game.board.pick_string(i)
+    if (i % 9).zero?
+      puts
+    elsif (i % 3).zero?
+      print '|'
+    end
+  end
+end
+
+def user_message(str)
+  print(str)
+end
 
 puts "\n\nWelcome to Tic Tac Toe!" \
      "\n\nUse your turn to place your mark on the board looking for" \
@@ -15,20 +48,20 @@ puts "\n\n => Enter 0 to exit <= \n\n"
 while game.continue != 'N'
   loop do
     puts "\n\nIt's turn for #{game.current_player}\n\n"
-    Interface.board_render(game)
+    board_render(game)
     print "\n\n#{game.players[game.current_player].name}, please choose a position: "
-    position = Interface.read_position(game)
+    position = read_position(game)
     break if position.to_i.zero?
 
     game.board.store_position(position, game.current_player)
     if game.winner?(position)
-      Interface.board_render(game)
+      board_render(game)
       puts "\n\nCongratulations #{game.players[game.current_player].name}, you won!\n\n"
       game.players[game.current_player].increase_score
       break
     end
     if game.board.full?
-      Interface.board_render(game)
+      board_render(game)
       puts "\n\n=> We have a draw between #{game.players['X'].name} and #{game.players['0'].name}!\n\n"
       break
     end
