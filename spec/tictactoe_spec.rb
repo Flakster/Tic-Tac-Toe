4065cc7a7f718f0650dd14b47f1a01456df15074
flakster <carlos.santamaria@gmail.com>
2020-03-10 21:40:45 -0500

    fix errors reported by Rubocop

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -3,8 +3,7 @@ require_relative '../lib/game'
 require_relative '../lib/board'
 require_relative '../lib/player'
 RSpec.describe Game do
-
-  let(:game) {Game.new}
+  let(:game) { Game.new }
 
   describe '#switch_player' do
     it 'switches the current player and returns true' do
@@ -15,15 +14,15 @@ RSpec.describe Game do
 
   describe '#two_d_array' do
     it 'returns a two-dimensional array from the positions' do
-      game.board.store_position('1','X')
-      game.board.store_position('2','0')
-      game.board.store_position('3','X')
-      game.board.store_position('4','0')
-      game.board.store_position('5','X')
-      game.board.store_position('6','0')
-      game.board.store_position('7','X')
-      game.board.store_position('8','0')
-      game.board.store_position('9','X')
+      game.board.store_position('1', 'X')
+      game.board.store_position('2', '0')
+      game.board.store_position('3', 'X')
+      game.board.store_position('4', '0')
+      game.board.store_position('5', 'X')
+      game.board.store_position('6', '0')
+      game.board.store_position('7', 'X')
+      game.board.store_position('8', '0')
+      game.board.store_position('9', 'X')
       expect(game.two_d_array).to eql([%w[X 0 X], %w[0 X 0], %w[X 0 X]])
     end
   end
@@ -31,7 +30,7 @@ RSpec.describe Game do
   describe '#winner?' do
     it 'returns true when there is a winner column, row or diagonal' do
       ar = [['X', '0', nil], ['X', '0', nil], ['X', '0', nil]]
-      game.win_col?(ar,0)
+      game.win_col?(ar, 0)
       expect(game.winner?(1)).to be true
     end
   end
@@ -39,14 +38,14 @@ RSpec.describe Game do
   describe '#win_col?' do
     it 'returns true if the column is a winner column' do
       ar = [['X', '0', nil], ['X', '0', nil], ['X', '0', nil]]
-      expect(game.win_col?(ar,0)).to be true
+      expect(game.win_col?(ar ,0)).to be true
     end
   end
 
   describe '#win_row?' do
     it 'returns true if the row is a winner row' do
       ar = [['X', '0', nil], %w[X X X], ['X', '0', nil]]
-      expect(game.win_row?(ar,1)).to be true
+      expect(game.win_row?(ar, 1)).to be true
     end
   end
 
@@ -78,38 +77,36 @@ RSpec.describe Game do
       expect(game.display_scores).to eql 'A: 0 - B: 0'
     end
   end
-
 end
 
 RSpec.describe Board do
-
-  let(:board) {Board.new}
+  let(:board) { Board.new }
 
   describe '#store_position' do
     it 'place the mark of the player in the given position' do
-      board.store_position('1','X')
+      board.store_position('1', 'X')
       expect(board.positions[0]).to eql 'X'
     end
   end
 
   describe '#full?' do
     it 'returns true if all the positions of the board are full' do
-      board.store_position('1','X')
-      board.store_position('2','0')
-      board.store_position('3','X')
-      board.store_position('4','0')
-      board.store_position('5','X')
-      board.store_position('6','0')
-      board.store_position('7','X')
-      board.store_position('8','0')
-      board.store_position('9','X')
+      board.store_position('1', 'X')
+      board.store_position('2', '0')
+      board.store_position('3', 'X')
+      board.store_position('4', '0')
+      board.store_position('5', 'X')
+      board.store_position('6', '0')
+      board.store_position('7', 'X')
+      board.store_position('8', '0')
+      board.store_position('9', 'X')
       expect(board.full?).to be true
     end
   end
 
   describe '#taken?' do
     it 'returns true if the given position is already set' do
-      board.store_position('1','0')
+      board.store_position('1', '0')
       expect(board.taken?('1')).to be true
     end
   end
@@ -123,7 +120,7 @@ RSpec.describe Board do
   describe '#pick_string' do
     it 'returns spaces for an empty space in the big board' do
       expect(board.pick_string(37)).to eql '  '
-    end 
+    end
 
     it 'returns two underscores if the number corresponds to a line' do
       expect(board.pick_string(20)).to eql '__'
@@ -137,7 +134,6 @@ RSpec.describe Board do
       board.store_position(1, 'X')
       expect(board.pick_string(11)).to eql ' X'
     end
-
   end
 
   describe '#number_placeholder?' do
